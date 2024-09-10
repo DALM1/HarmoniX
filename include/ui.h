67941858888d5f4fb5d919677e0240cf8eabb5f6
Dimitri Almon <dimitri.almon@gmail.com>
2024-09-10 16:03:14 +0200

    Update ui and debugging the playset

--- include/ui.h
+++ include/ui.h
@@ -2,10 +2,24 @@
 #define UI_H
 
 #include <gtk/gtk.h>
+#include <gst/gst.h>
 
-void on_activate(GtkApplication *app, gpointer user_data);
-void on_music_button_clicked(GtkWidget *widget, gpointer window);
+extern GstElement *pipeline;
+
+GtkWidget* create_icon_bar();
+GtkWidget* create_icon_button(const char *icon_path);
+void on_play_button_clicked(GtkWidget *widget, gpointer data);
+void on_pause_button_clicked(GtkWidget *widget, gpointer data);
+void on_skip_button_clicked(GtkWidget *widget, gpointer data);
+void on_prev_button_clicked(GtkWidget *widget, gpointer data);
 void on_playlist_button_clicked(GtkWidget *widget, gpointer window);
+void on_music_button_clicked(GtkWidget *widget, gpointer window);
 void on_folder_button_clicked(GtkWidget *widget, gpointer window);
-GtkWidget* create_horizontal_icon_bar();
+void on_activate(GtkApplication *app, gpointer user_data);
+void apply_css(GtkWidget *widget);
+GtkWidget* create_player_box();
+gboolean update_progress_bar(gpointer data);
+void refresh_music_list();
+void play_music_by_index(gint index);
+
 #endif
--- src/main.c
+++ src/main.c
@@ -5,10 +5,8 @@ int main(int argc, char **argv) {
     GtkApplication *app;
     int status;
 
-
     app = gtk_application_new("com.harmonix.app", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
-
 
     status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
